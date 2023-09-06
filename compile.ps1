20c70c8f414244ebcb27488805e0e85bdc417bbf
ContainedBlargh <jon@zendata.dk>
2023-09-06 22:59:31 +0200

    remove clutter

--- compile.ps1
+++
@@ -1,7 +0,0 @@
-$CC="gcc"
-$CFLAGS="-std=c2x -Wall -O3 -Wno-unknown-pragmas"
-$LIBS="-lSDL2 -lSDL2main"
-$TARGET="craytracer.exe"
-
-cbundl .\src\main.c .\bundle.c
-Invoke-Expression "$CC $CFLAGS bundle.c -o $TARGET $LIBS"
--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gcc -std=c2x -Wall -O3 fail.c util.c vec3.c sphere.c main.c -o craytracer -lSDL2 -lSDL2main
